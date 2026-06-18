9c1d3fd2a2308ea10bda86b399fd9c3d248be8cb
Oktora15 <nitoruciane@gmail.com>
2026-06-18 11:29:30 +0700

    git commit -m "Memindahkan file Pertemuan9.php ke folder Content dan menambahkan file config.php" git push

---
+++ app/config.php
@@ -0,0 +1,24 @@
+<?php
+
+// dekorasi variable mysql
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "kampusq";
+
+$connection = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+if (!$connection) {
+    echo '
+    <script>
+        alert("Koneksi dengan database berhasil");
+    </script>';
+} else {
+    echo '
+    <script>
+        alert("Koneksi dengan database gagal: ' . mysqli_connect_error() . '");
+    </script>';
+}
+
+
+?>
